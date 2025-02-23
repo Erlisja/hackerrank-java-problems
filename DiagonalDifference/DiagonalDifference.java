2eaf2adf657cc9be8ef7242d511759bfff305278
Erlisja Kore <erlisja.kore@temple.edu>
2025-02-23 00:09:55 -0500

    diagonal difference

--- DiagonalDifference/DiagonalDifference.java
+++ DiagonalDifference/DiagonalDifference.java
@@ -1,5 +1,57 @@
 package Hackerrank_Problems.DiagonalDifference;
 
-public class DiagonalDifference {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
     
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+     int rDiag = 0;
+    int lDiag = 0;
+    for (int i = 0; i<arr.size(); i++){
+        rDiag += arr.get(i).get(i);
+        lDiag += arr.get(i).get(arr.size()-i-1);
+    }
+    return Math.abs(lDiag-rDiag);
+
+    }
+}
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+       // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+        System.out.println(result);
+    }
 }
